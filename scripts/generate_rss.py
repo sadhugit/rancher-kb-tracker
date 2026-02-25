cb103c3ee28678937fef6229a1898cb2ddd3f51f
Sadhu <ranchersadhu@gmail.com>
2026-02-25 12:20:03 +0530

    Change repository name in generate_rss.py

--- scripts/generate_rss.py
+++ scripts/generate_rss.py
@@ -3,7 +3,8 @@ from feedgen.feed import FeedGenerator
 from datetime import datetime, timezone
 import os
 
-REPO = "sadhugit/rancher-kb-tracker"
+#REPO = "sadhugit/rancher-kb-tracker"
+REPO = "rancherlabs/support-kb"
 TOKEN = os.getenv("GITHUB_TOKEN")
 headers = {}
 if TOKEN:
